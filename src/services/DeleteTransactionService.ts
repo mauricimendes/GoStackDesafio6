16c3a5791b09b28ff575c0cccc681f3e05090e63
mauricimendes <mauricimendes.14@gmail.com>
2020-06-26 12:34:30 -0300

    desafio 6 completo, graças a deus

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm'
+
+import AppError from '../errors/AppError';
+import TransactionRespository from '../repositories/TransactionsRepository'
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRespository = getCustomRepository(TransactionRespository)
+
+    const transaction = await transactionsRespository.findOne(id)
+
+    if(!transaction) {
+      throw new AppError('Transaction does not exist')
+    }
+
+    await transactionsRespository.remove(transaction)
   }
 }
 
